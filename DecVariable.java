12f20b28a1cc441a5bb506b7df4c40cbe8b516f8
Ritesh786 <jharitesh6@gmail.com>
2016-07-13 14:45:00 +0530

    session 1 ,assignment 2 commit
    session 1 ,assignment 2 commit

---
+++ DecVariable.java
@@ -0,0 +1,12 @@
+
+public class DecVariable {
+
+	public static void main(String[] args) {
+		int a,b=4,c;
+        a=1;
+        c=3;
+        System.out.println("value of a,b,c " +a+b+c);
+        
+	}
+
+}
